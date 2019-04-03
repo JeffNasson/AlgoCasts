05dab8a185bf74d39df994210296490b1862631a
jeffrey <jeffrey.nasson@gmail.com>
2019-04-03 13:28:58 -0600

    reversed done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+function reverse(str){
+    return str.split('').reduce((reversed,character)=>{
+        return character + reversed;
+    },'');
+}
 
 module.exports = reverse;
+
+// SOLUTIONS //
+
+// function reverse(str) {
+//     // let newStr = str.split('');
+//     // newStr.reverse();
+//     // return newStr.join('');
+//         // both work
+//     return str.split('').reverse().join('');
+// }
+
+
+// function reverse(str){
+//     let reversed = '';
+
+//     for(let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
